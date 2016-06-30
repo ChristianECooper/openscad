f7ceea85a02320f9a9c020f390fa8dffbd654d27
Marius Kintel <marius@kintel.net>
2016-06-30 14:29:55 -0400

    Install imagemagick, link gettext

--- scripts/macosx-build-homebrew.sh
+++ scripts/macosx-build-homebrew.sh
@@ -24,10 +24,11 @@ brew tap openscad/tap
 brew tap glehmann/homebrew-extras
 brew update
 brew unlink boost
-for formula in eigen boost cgal glew glib opencsg freetype libxml2 fontconfig harfbuzz qt5 qscintilla2-qt5; do
+for formula in eigen boost cgal glew glib opencsg freetype libxml2 fontconfig harfbuzz qt5 qscintilla2-qt5 imagemagick; do
   brew ls --versions $formula && brew install $formula
   brew outdated $formula || brew upgrade $formula
 done
+brew link --force gettext
 brew link --force qt5
 brew link --force qscintilla2-qt5
 
